49d4cc9a3796a4c0d9cfebd5a3ee839b0426f240
Martin Smith <msmith92663@gmail.com>
2023-03-13 14:47:57 -0700

    added sample files

---
+++ include/Filter.h
@@ -0,0 +1,29 @@
+C#ifndef __GST_ARFILTER_FILTER__
+#define __GST_ARFILTER_FILTER__
+
+#include <string>
+#include "opencv2/opencv.hpp"
+#include "gst/gst.h"
+
+
+class Filter
+{
+private:
+	std::string &name;
+	GstBus* bus;
+	std::string parameters;
+public:
+	Filter(std::string&);
+	virtual void process(cv::Mat&) = 0;
+	std::string& getName();
+	void setBus(GstBus* bus);
+	void post(GstStructure *);
+	void postBooleanEvent(std::string eventName, bool eventData);
+	void postStringEvent(std::string eventName, std::string eventData);
+	void postIntEvent(std::string eventName, int eventData);
+	void postDoubleEvent(std::string eventName, double eventData);
+	virtual void setParameters(std::string& parameters);
+	std::string getParameters();
+};
+
+#endif /* __GST_ARFILTER_FILTER__ */
